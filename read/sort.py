07ff3aee23ec1f6b80661924074140fcf653c1c7
Dan Saunders <djsaunde@umass.edu>
2018-07-19 12:00:19 -0400

    Updating reading list.

--- read/sort.py
+++ read/sort.py
@@ -13,7 +13,7 @@ def main():
 
 		try:
 			df = pd.read_csv(f, parse_dates=['Read'])
-			df = df.sort_values(by='Read', axis='rows', ascending=True)
+			df = df.sort_values(by='Read', axis='rows', ascending=False)
 			df.to_csv(f, index=False)
 		except pd.errors.EmptyDataError:
 			continue
